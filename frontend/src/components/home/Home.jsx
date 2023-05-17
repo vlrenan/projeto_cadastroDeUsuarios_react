e51961ae7523b3afb1d76bfe884c29944a1e0338
vlrenan <renan.silva@aluno.faculdadeimpacta.com.br>
2023-05-17 15:57:37 -0300

    add

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Desenvolvido por Renan.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de cadastro, desenvolvido em React!</p>
+    </Main>
